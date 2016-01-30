e1ae716cd2a339053149ccd3a7c41fc3824e3236
Jonathan Palmer (Capstone) <jkp2377@IGMGRAD-01.main.ad.rit.edu>
2016-01-29 22:34:21 -0500

    Setting up player UI and player abilities.

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ability : ScriptableObject 
+{
+	public Player owner;
+
+	public bool initialized = false;
+	public void Init(Player newOwner)
+	{
+		if (!initialized)
+		{
+			//Add UI to the controlling player
+			owner = newOwner;
+
+
+
+			initialized = true;
+		}
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour 
 {
+
+	public List<Ability> abilities;
+
 
 	void Start () 
 	{
