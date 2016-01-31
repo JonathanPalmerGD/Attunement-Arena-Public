08775654cfda84bbe8e09d89b46dce189ebcc847
Jonathan Palmer (Capstone) <jkp2377@rit.edu>
2016-01-31 14:43:54 -0500

    Audio progress. Fixed water shield and chilled particles. Being chilled slows you. Working on music and bolt.

--- Assets/Scripts/Abilities/WaterShield.cs
+++ Assets/Scripts/Abilities/WaterShield.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class WaterShield : Ability
 {
-	public GameObject waterShieldPrefab;
-	public GameObject shieldParticle;
-
 	public override KeyActivateCond activationCond
 	{
 		get { return KeyActivateCond.KeyDown; }
@@ -38,37 +35,19 @@ public class WaterShield : Ability
 		}
 		else
 		{
-			if (shieldParticle.activeSelf)
-			{
-				shieldParticle.SetActive(false);
-			}
 			//Debug.Log("Updating Gust - Grounded\n" + deltaTime);
 			base.UpdateAbility(deltaTime);
 		}
 	}
 
-	public override void Init(Player newOwner, string newKeyBinding, string displayKeyBinding)
-	{
-		waterShieldPrefab = Resources.Load<GameObject>("waterShieldPrefab");
-		shieldParticle = GameObject.Instantiate(waterShieldPrefab, newOwner.transform.position, newOwner.transform.rotation) as GameObject;
-		shieldParticle.name = "Bolt Renderer [P" + newOwner.playerID + "]";
-		
-		shieldParticle.transform.SetParent(newOwner.transform);
-		base.Init(newOwner, newKeyBinding, displayKeyBinding);
-	}
-
 	public override void ExecuteAbility(Vector3 inputVector = default(Vector3))
 	{
 		var castDir = inputVector.normalized;
-
-		shieldParticle.SetActive(true);
-
+		
 		//Debug.DrawLine(Owner.transform.position, Owner.transform.position + castDir * Range, Color.green, 5.0f);
 
-		if (Owner.buffState != Player.PlayerBuff.Shielded)
-		{
-			durCounter = Duration;
-			Owner.buffState = Player.PlayerBuff.Shielded;
-		}
+		Debug.Log("Shield Player\n");
+		durCounter = Duration;
+		Owner.buffState = Player.PlayerBuff.Shielded;
 	}
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -52,6 +52,7 @@ public class Player : MonoBehaviour
 	public Scrollbar hpBar;
 	public Scrollbar mpBar;
 	public ParticleSystem chilledParticles;
+	public ParticleSystem shieldParticles;
 	#endregion
 
 	#region Properties
@@ -207,6 +208,8 @@ public class Player : MonoBehaviour
 				Cursor.visible = false;
 				Cursor.lockState = CursorLockMode.Locked;
 			}
+
+			shieldParticles.enableEmission = false;
 		}
 	}
 
@@ -266,28 +269,35 @@ public class Player : MonoBehaviour
 
 	public void UpdateBuffState()
 	{
+		#if UNITY_EDITOR
 		if (Input.GetKeyDown(KeyCode.L))
 		{
 			SetChilledState(5);
 		}
+		#endif
 
-		if (buffState != PlayerBuff.None)
+		if (buffState == PlayerBuff.Chilled)
 		{
-			if (buffState == PlayerBuff.Chilled)
-			{
-				remainingStatDur -= Time.deltaTime;
-				AdjustHealth(-2 * Time.deltaTime, false);
-			}
-
+			remainingStatDur -= Time.deltaTime;
+			AdjustHealth(-2 * Time.deltaTime, false);
 			if (remainingStatDur <= 0)
 			{
 				remainingStatDur = 0;
 				buffState = PlayerBuff.None;
 			}
 		}
-		else
+		
+		if (buffState != PlayerBuff.Chilled)
 		{
 			chilledParticles.enableEmission = false;
+		}
+		if (buffState == PlayerBuff.Shielded)
+		{
+			shieldParticles.enableEmission = true;
+		}
+		else
+		{
+			shieldParticles.enableEmission = false;
 		}
 	}
 
@@ -483,6 +493,12 @@ public class Player : MonoBehaviour
 			//Add it to displayed Mana
 			Mana += gainThisFrame;
 		}
+	}
+
+	public void SetShieldedState(float chillDur)
+	{
+		shieldParticles.enableEmission = true;
+		buffState = PlayerBuff.Shielded;
 	}
 
 	public void SetChilledState(float chillDur)
